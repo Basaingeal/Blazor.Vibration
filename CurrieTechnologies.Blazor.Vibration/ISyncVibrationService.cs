ba90c2467622929b7e05927e280aa3040cacbd83
Michael J. Currie <michaeljcurrie136@gmail.com>
2019-06-18 14:47:51 -0500

    initial commit

---
+++ CurrieTechnologies.Blazor.Vibration/ISyncVibrationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrieTechnologies.Blazor.Vibration
+{
+    interface ISyncVibrationService
+    {
+    }
+}
---
+++ CurrieTechnologies.Blazor.Vibration/VibrationService.cs
@@ -0,0 +1,9 @@
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace CurrieTechnologies.Blazor.Vibration
+{
+    public class VibrationService: IAsyncVibrationService, ISyncVibrationService
+    {
+    }
+}
